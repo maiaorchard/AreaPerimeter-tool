a20898f71ab579a403c9f0e7aced2de4449d3fa7
maiaorchard <125936620+maiaorchard@users.noreply.github.com>
2023-08-20 22:04:53 +0000

    Shape selector finished

--- shapesfunction.py
+++ shapesfunction.py
@@ -1,33 +1,36 @@
-f = True
 
 #Functions go at the top
 def shape_picker(question):
-  shape_selected = input(question).strip().lower()
-  if shape_selected == shapes[0]:
-    print(shape_selected)
-    #ask for measurements 
-  elif shape_selected == shapes[1]:
-    triangle_type = input("Which type of triangle? ").strip().lower()
-    #ask for measurements 
-    print(triangle_type)
-  elif shape_selected == shapes[2]:
-    print(shape_selected)
-    #ask for measurements 
-  elif shape_selected == shapes[3]:
-    print(shape_selected)
-    #ask for measurements 
-  elif shape_selected == shapes[4]:
-    print(shape_selected)
-    #ask for measurements 
-  else:
-    print("That shape is not in the list. Please enter a different shape")
-    shape_selected = input(question)
+  valid_shape = False
+  while valid_shape == False:
+    shape_selected = input(question).strip().lower()
+    if shape_selected == shapes[0]:
+      valid_shape = True
+       #ask for measurements 
+
+    elif shape_selected == shapes[1]:
+      valid_shape = True
+      triangle_type = input("Which type of triangle? ").strip().lower()
+      #ask for measurements 
+    
+    elif shape_selected == shapes[2]:
+      valid_shape = True
+      #ask for measurements 
+
+    elif shape_selected == shapes[3]:
+      valid_shape = True
+      #ask for measurements 
+
+    elif shape_selected == shapes[4]:
+      valid_shape = True
+      #ask for measurements 
+    else:
+      print("That shape is not in the list. Please enter a different shape")
 
 #Lists go under functions
 shapes = ["square", "triangle", "rectangle", "circle", "parallelogram"]
 triangles = ["right angle", "equilateral", "isoceles", "acute", "scalene", "obtuse"]
 
 #Use the function
-while f == True:
-  shape_picker("Choose a shape: ")
+shape_picker("Choose a shape: ")
 
