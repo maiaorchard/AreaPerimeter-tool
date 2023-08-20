f094c247b54ee4d1de764fbfa1f37d3a906d4809
maiaorchard <125936620+maiaorchard@users.noreply.github.com>
2023-08-20 21:31:16 +0000

    Shape selector testing version

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
 
-import shapesfunction.py
+import shapesfunction
--- shapesfunction.py
+++ shapesfunction.py
@@ -1,12 +1,33 @@
+f = True
 
 #Functions go at the top
 def shape_picker(question):
   shape_selected = input(question).strip().lower()
   if shape_selected == shapes[0]:
-    length = input("What is the length of your shape?")
+    print(shape_selected)
+    #ask for measurements 
+  elif shape_selected == shapes[1]:
+    triangle_type = input("Which type of triangle? ").strip().lower()
+    #ask for measurements 
+    print(triangle_type)
+  elif shape_selected == shapes[2]:
+    print(shape_selected)
+    #ask for measurements 
+  elif shape_selected == shapes[3]:
+    print(shape_selected)
+    #ask for measurements 
+  elif shape_selected == shapes[4]:
+    print(shape_selected)
+    #ask for measurements 
+  else:
+    print("That shape is not in the list. Please enter a different shape")
+    shape_selected = input(question)
 
 #Lists go under functions
 shapes = ["square", "triangle", "rectangle", "circle", "parallelogram"]
+triangles = ["right angle", "equilateral", "isoceles", "acute", "scalene", "obtuse"]
 
 #Use the function
-shape_picker("Choose a shape: ")
+while f == True:
+  shape_picker("Choose a shape: ")
+
