bb01c9e2328dcc37e0fb60e75e01447ff412c46e
maiaorchard <125936620+maiaorchard@users.noreply.github.com>
2023-08-23 01:18:26 +0000

    ask for measurements for all triangle types done

--- shapesfunction.py
+++ shapesfunction.py
@@ -4,6 +4,7 @@ def shape_picker(question):
   valid_shape = False
   while valid_shape == False:
     shape_selected = input(question).strip().lower()
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     if shape_selected == shapes[0]:
       valid_shape = True
       
@@ -18,42 +19,153 @@ def shape_picker(question):
       
       #ask for units
       #units = input("Please enter the units: ")
-
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     elif shape_selected == shapes[1]:
       valid_shape = True
       valid_triangle = False
       while valid_triangle == False:
         triangle_type = input("Which type of triangle? ").strip().lower()
-      
+      #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         if triangle_type == triangles[0]:
           valid_triangle = True
-          #ask for measurements depending on the formulas
-          
-          
+          #ask for length
+          valid_length = False
+          while valid_length == False:
+            try:
+              length = float(input("What is the length of the triangle? (Please enter only a number) "))
+              valid_length = True
+            except:
+              print("Please enter a number")
+
+          #ask for width
+          valid_width = False
+          while valid_width == False:
+            try:
+              width = float(input("What is the width of the triangle? (Please enter only a number) "))
+              valid_width = True
+            except:
+              print("Please enter a number")
+      #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         elif triangle_type == triangles[1]:
           valid_triangle = True
-          #ask for other measurements depending on the formulas
-          
+          #ask for length
+          valid_length = False
+          while valid_length == False:
+            try:
+              length = float(input("What is the length of the triangle? (Please enter only a number) "))
+              valid_length = True
+            except:
+              print("Please enter a number")
+      #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         elif triangle_type == triangles[2]:
           valid_triangle = True
-          #ask for other measurements depending on the formulas
-          
+          #ask for base length
+          valid_base = False
+          while valid_base == False:
+            try:
+              base = float(input("What is the length of the base of the triangle? (Please enter only a number) "))
+              valid_base = True
+            except:
+              print("Please enter a number")
+
+          #ask for height
+          valid_height = False
+          while valid_height == False:
+            try:
+              height = float(input("What is the height of the triangle? (Please enter only a number) "))
+              valid_height = True
+            except:
+              print("Please enter a number")
+
+          #ask for the length of the side
+          valid_length = False
+          while valid_length == False:
+            try:
+              side = float(input("What is the length of the side of the triangle? (Please enter only a number) "))
+              valid_length = True
+            except:
+              print("Please enter a number")
+      #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         elif triangle_type == triangles[3]:
           valid_triangle = True
-          #ask for other measurements depending on the formulas
-          
-        elif triangle_type == triangles[4]:
+          #ask for base measurement
+          valid_base = False
+          while valid_base == False:
+            try:
+              base = float(input("What is the length of the base of the triangle? (Please enter only a number) "))
+              valid_base = True
+            except:
+              print("Please enter a number")
+
+          #ask for height
+          valid_height = False
+          while valid_height == False:
+            try:
+              height = float(input("What is the height of the triangle? (Please enter only a number) "))
+              valid_height = True
+            except:
+              print("Please enter a number")
+
+          #ask for the length of one side
+          valid_length = False
+          while valid_length == False:
+            try:
+              side = float(input("What is the length of one side of the triangle? (Please enter only a number) "))
+              valid_length = True
+            except:
+              print("Please enter a number")
+
+          #ask for the length of the other side
+          valid_length2 = False
+          while valid_length2 == False:
+            try:
+              side = float(input("What is the length of the other side of the triangle? (Please enter only a number) "))
+              valid_length2 = True
+            except:
+              print("Please enter a number")
+    
+        elif triangle_type == triangles[4] or triangle_type == triangles[5]:
           valid_triangle = True
-          #ask for other measurements depending on the formulas
-          
-        elif triangle_type == triangles[5]:
-          valid_triangle = True
-          #ask for other measurements depending on the formulas
-          
+          #ask for base measurement
+          valid_base = False
+          while valid_base == False:
+            try:
+              base = float(input("What is the length of the base of the triangle? (Please enter only a number) "))
+              valid_base = True
+            except:
+              print("Please enter a number")
+
+          #ask for height
+          valid_height = False
+          while valid_height == False:
+            try:
+              height = float(input("What is the height of the triangle? (Please enter only a number) "))
+              valid_height = True
+            except:
+              print("Please enter a number")
+
+          #ask for the length of one side
+          valid_length = False
+          while valid_length == False:
+            try:
+              side = float(input("What is the length of one side of the triangle? (Please enter only a number) "))
+              valid_length = True
+            except:
+              print("Please enter a number")
+
+          #ask for the length of the other side
+          valid_length2 = False
+          while valid_length2 == False:
+            try:
+              side = float(input("What is the length of the other side of the triangle? (Please enter only a number) "))
+              valid_length2 = True
+            except:
+              print("Please enter a number")
+      #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         else:
           valid_triangle = False
-          print("Please enter a type of triangle.")
-    
+          print("Please enter a type of triangle or check your spelling of the triangle type.")
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     elif shape_selected == shapes[2]:
       valid_shape = True
       #ask for length
@@ -73,7 +185,7 @@ def shape_picker(question):
           except:
             print("Please enter a number")
             #ask for units
-
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     elif shape_selected == shapes[3]:
       valid_shape = True
       #ask for radius
@@ -85,7 +197,7 @@ def shape_picker(question):
         except:
           print("Please enter a number")
       #ask for units 
-
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     elif shape_selected == shapes[4]:
       valid_shape = True
       #ask for base length
@@ -111,12 +223,13 @@ def shape_picker(question):
       while valid_length == False:
         try:
           side = float(input("What is the length of the side of the parallelogram? (Please enter only a number) "))
-          valid_number = True
+          valid_length = True
         except:
           print("Please enter a number")
-    
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     else:
       print("That shape is not in the list. Please enter a different shape or  check your spelling of the shape")
+
 
 #Lists go under functions
 shapes = ["square", "triangle", "rectangle", "circle", "parallelogram"]
